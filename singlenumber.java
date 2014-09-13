b37fa55ef3d0bc6e4c6c91e781b632a672307ced
Allen Zhao <cnallenzhao@gmail.com>
2014-09-13 15:18:59 +0800

    Code style

--- singlenumber.java
+++ singlenumber.java
@@ -8,14 +8,14 @@
 public class Solution {
 	public int singleNumber(int[] A) {
 		// Do the check to see if A is null or nothing is in A.
-		if(A == null || A.length == 0) {
+		if (A == null || A.length == 0) {
 			return -1;
 		}
 		
 		// Do the XOR loop.
 		int single = 0;
 
-		for(int i = 0; i < A.length; i++) {
+		for (int i = 0; i < A.length; i++) {
 			single ^= A[i];
 		}
 		return single;
