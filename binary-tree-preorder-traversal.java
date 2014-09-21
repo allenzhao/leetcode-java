4e13b49c6f19fc6f5b5a3159b183687236d521fe
Allen Zhao <cnallenzhao@gmail.com>
2014-09-21 22:30:10 +0800

    Adding binary tree traversal

---
+++ binary-tree-preorder-traversal.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        traverse(root,result);
+        return result;
+    }
+
+    private void traverse(TreeNode root, ArrayList<Integer> result) {
+        if (root == null) {
+            return;
+        }
+
+        result.add(root.val);
+        traverse(root.left, result);
+        traverse(root.right, result);
+    }
+}
